75e2f954bbbefb76f3476f660b7727dc6af5d7c4
aakumykov <aakumykov@yandex.ru>
2025-12-16 14:26:50 +0300

    Вложенные элементы. При повороте экрана наблюдатели жизненного цикла вложенных Compose-элементов тоже не перезапускаются.

--- app/src/main/java/com/github/aakumykov/compose_playground/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/compose_playground/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.github.aakumykov.compose_playground.ui.theme.Compose_playgroundTheme
 
@@ -33,11 +40,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val myRememberObserver = remember { MyRememberObserver() }
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    val myRememberObserver = remember { MyRememberObserver("Greeting") }
+    Column(modifier = modifier.fillMaxSize().background(Color.Cyan)) {
+        Text(name)
+        var checked by remember { mutableStateOf(false) }
+        Checkbox(checked = checked, onCheckedChange = { checked = it })
+        if (checked) {
+            val myObject = remember { MyRememberObserver("checked") }
+        }
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/github/aakumykov/compose_playground/MyRememberObserver.kt
+++ app/src/main/java/com/github/aakumykov/compose_playground/MyRememberObserver.kt
@@ -3,22 +3,22 @@ package com.github.aakumykov.compose_playground
 import android.util.Log
 import androidx.compose.runtime.RememberObserver
 
-class MyRememberObserver : RememberObserver {
+class MyRememberObserver(private val label: String) : RememberObserver {
 
     init {
-        Log.d(TAG, "[${hashCode()}] init{}")
+        Log.d(TAG, "($label) [${hashCode()}] init{}")
     }
 
     override fun onRemembered() {
-        Log.d(TAG, "[${hashCode()}] onRemembered")
+        Log.d(TAG, "($label) [${hashCode()}] onRemembered")
     }
 
     override fun onForgotten() {
-        Log.d(TAG, "[${hashCode()}] onForgotten")
+        Log.d(TAG, "($label) [${hashCode()}] onForgotten")
     }
 
     override fun onAbandoned() {
-        Log.d(TAG, "[${hashCode()}] onAbandoned")
+        Log.d(TAG, "($label) [${hashCode()}] onAbandoned")
     }
 
     companion object {
