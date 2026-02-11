f58fdddd28c17de169773d8dbf1d0dad37951f7f
aakumykov <aakumykov@yandex.ru>
2026-02-11 15:37:48 +0300

    По умолчанию поля ввода не переключаются табуляцией.

--- app/src/main/java/com/github/aakumykov/compose_playground/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/compose_playground/MainActivity.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.github.aakumykov.compose_playground.ui.theme.Compose_playgroundTheme
@@ -32,10 +39,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+        var name by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+
+        TextField(
+            value = name,
+            label = { "имя" },
+            onValueChange = { name = it },
+//            modifier = modifier.fillMaxWidth()
+        )
+        TextField(
+            value = password,
+            label = { "пароль" },
+            onValueChange = { password = it},
+//            modifier = modifier.fillMaxWidth()
+        )
+    }
 }
 
 @Preview(showBackground = true)
