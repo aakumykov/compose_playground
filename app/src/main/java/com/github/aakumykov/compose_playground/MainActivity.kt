d1e7dff3b493ef12e67d852d42a288f8028e3a4b
aakumykov <aakumykov@yandex.ru>
2026-02-12 11:43:42 +0300

    Навигация табуляцией между полями ввода в этом примере есть.

--- app/src/main/java/com/github/aakumykov/compose_playground/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/compose_playground/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.github.aakumykov.compose_playground.ui.theme.Compose_playgroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +29,44 @@ class MainActivity : ComponentActivity() {
         setContent {
             Compose_playgroundTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ColumnOfInputs(innerPadding)
                 }
             }
         }
     }
 }
 
+//
+// https://github.com/JetBrains/compose-multiplatform/blob/master/tutorials/Tab_Navigation/README.md
+//
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ColumnOfInputs(innerPadding: PaddingValues) {
+    Box(
+        modifier = Modifier.fillMaxSize().padding(innerPadding),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier.padding(50.dp)
+        ) {
+            repeat(5) {
+                val text = remember { mutableStateOf("") }
+                OutlinedTextField(
+                    value = text.value,
+                    singleLine = true,
+                    onValueChange = { text.value = it }
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+            }
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Compose_playgroundTheme {
-        Greeting("Android")
+        ColumnOfInputs(PaddingValues(0.dp))
     }
 }
