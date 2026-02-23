0254d8daa629564504ac631cbd0b404f893a79d4
Andrey Kumykov <aakumykov@yandex.ru>
2026-02-23 12:21:36 +0300

    Строка прогресса с текстовым индикатором.

--- app/src/main/java/com/github/aakumykov/compose_playground/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/compose_playground/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.github.aakumykov.compose_playground
 
 import android.os.Bundle
+import android.widget.ProgressBar
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.github.aakumykov.compose_playground.ui.theme.Compose_playgroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +40,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
-    LinearDeterminateIndicator(modifier = modifier)
+//    LinearDeterminateIndicator(modifier = modifier)
+    val progress = 0.6f
+    Row (
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        LinearProgressIndicator(
+            progress = { progress },
+            modifier = Modifier.fillMaxWidth().weight(1f, true)
+        )
+        Text(
+            text = "${progress}%",
+            fontSize = 12.sp,
+            modifier = Modifier.padding(start = 8.dp, end = 4.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
