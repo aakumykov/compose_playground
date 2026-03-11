c3dc5a04318655b6eefcae57dedaad6d9d83d2b1
aakumykov <aakumykov@yandex.ru>
2026-03-11 12:44:25 +0300

    Revert "Поигрался с тестовыми полями."
    This reverts commit eb5da0dfec110e055baaa5d6313fbffb58f09b2c.

--- app/src/main/java/com/github/aakumykov/compose_playground/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/compose_playground/MainActivity.kt
@@ -6,23 +6,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.input.rememberTextFieldState
-import androidx.compose.foundation.text.input.setTextAndPlaceCursorAtEnd
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -54,37 +42,12 @@ class MainActivity : ComponentActivity() {
 fun Greeting(modifier: Modifier = Modifier) {
 //    val scope = rememberCoroutineScope()
     val context = LocalContext.current
-
-    Column(
-        verticalArrangement = Arrangement.SpaceBetween,
-        modifier = modifier.fillMaxSize()
-    ) {
-
-        DropDownMenu3(
-            menuLabel = stringResource(R.string.drop_down_menu_label)
-            , options = fakeOptionList
-            , modifier = Modifier
-            , onOptionSelected = { value: String ->
-                Toast.makeText(context, value, Toast.LENGTH_SHORT).show()
-            }
-        )
-
-        val text = "Простой текст"
-        val textyState = rememberTextFieldState(text)
-        val textFieldModifier = Modifier.fillMaxWidth()
-        var simpleText by remember { mutableStateOf(text) }
-
-        TextField(
-            value = simpleText,
-            onValueChange = { simpleText = it },
-            modifier = textFieldModifier,
-            label = { Text("Простое поле", color = MaterialTheme.colorScheme.primary) }
-        )
-
-        OutlinedTextField(
-            state =textyState,
-            modifier = textFieldModifier,
-            label = { Text("Поле с каёмочкой", color = MaterialTheme.colorScheme.primary) }
-        )
-    }
+    DropDownMenu3(
+        menuLabel = stringResource(R.string.drop_down_menu_label)
+        , options = fakeOptionList
+        , modifier = modifier
+        , onOptionSelected = { value: String ->
+            Toast.makeText(context, value, Toast.LENGTH_SHORT).show()
+        }
+    )
 }
