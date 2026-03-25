ac7392e81d3883034a7c8a2e60f7626ff61001ac
aakumykov <aakumykov@yandex.ru>
2026-03-25 11:40:00 +0300

    Удалил randomString с использованием java-faker.

--- app/src/main/java/com/github/aakumykov/compose_playground/utils/random.kt
+++ app/src/main/java/com/github/aakumykov/compose_playground/utils/random.kt
@@ -1,13 +1,7 @@
 package com.github.aakumykov.compose_playground.utils
 
-import com.github.javafaker.Faker
-import java.util.Locale
 import kotlin.random.Random
 
 val random: Random get() = Random
-
-val faker: Faker by lazy { Faker(Locale.getDefault()) }
-
-val randomString: String get() = faker.address().cityName()
 
 val randomBool: Boolean get() = random.nextBoolean()
