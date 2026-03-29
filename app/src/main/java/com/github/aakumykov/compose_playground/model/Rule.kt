e88bca85c268a23112f83596fb8d2a035d4c6b6c
Andrey Kumykov <aakumykov@yandex.ru>
2026-03-29 21:08:08 +0300

    fix: значение по умолчанию для Rule().created; версия БД 4

--- app/src/main/java/com/github/aakumykov/compose_playground/model/Rule.kt
+++ app/src/main/java/com/github/aakumykov/compose_playground/model/Rule.kt
@@ -22,7 +22,7 @@ class Rule(
     @ColumnInfo(name = "rule_subject") val ruleSubject: RuleSubject,
     @ColumnInfo(name = "rule_operation") val ruleOperation: RuleOperation,
     @ColumnInfo(name = "check_pattern") val checkPattern: String,
-    val created: Long
+    @ColumnInfo(defaultValue = "0") val created: Long
 ) {
     companion object {
         const val TABLE_NAME = "rules"
